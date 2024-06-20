68b19c1a6648a6cade05020f0c1711ddeda4665a
dariorfm <dariorfm@gmail.com>
2024-06-20 13:20:33 +0200

    5.2_step_2 Now login is permanent and logout remove localStorage user details

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -17,6 +17,15 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
     
@@ -24,6 +33,10 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
 
       setUser(user)
       setUsername('')
@@ -64,7 +77,12 @@ const App = () => {
   const showBlogs = () => (
     <div>
       <h2>Blogs</h2>
-      <p>{user.name} logged-in</p>
+      <p>{user.name} logged-in
+      <button onClick={() => {
+        window.localStorage.removeItem('loggedBlogappUser')
+        setUser(null)
+      }}>logout</button>
+      </p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
